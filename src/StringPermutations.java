7fd0f383ceea38db86c6e8a81ffa5f8a6ebe1c92
Mannuru <arunm057@gmail.com>
2017-07-21 17:15:19 -0400

    countfrequencymethod

--- src/StringPermutations.java
+++ src/StringPermutations.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Hashtable;
 
 /**
@@ -27,6 +28,8 @@ public class StringPermutations {
 		int lenA = strA.length();
 		int lenB = strB.length();
 		System.out.println(isPermutation(strA,strB));
+		
+		System.out.println(compare(strA,strB));
 	}
 	/*
 	 * Permutations means rearrangements 
@@ -46,4 +49,51 @@ public class StringPermutations {
 		Arrays.sort(chB);
 		return Arrays.equals(chA, chB);
 	}
+	
+	//create a HashMap containing the frequencys of every character of the String  (runtime O(n) )
+	static HashMap<Character, Integer> getFrequencys(String s){
+	    HashMap<Character, Integer> map = new HashMap<>();
+
+	    for(int i=0; i<s.length(); i++){
+	        //get character at position i
+	        char c = s.charAt(i);
+
+	        //get old frequency (edited: if the character is added for the 
+	        //first time, the old frequency is 0)
+	        int frequency;
+	        if(map.containsKey(c)){
+	            frequency = map.get(c);
+	        }else{
+	            frequency = 0;
+	        }
+	        //increment frequency by 1
+	        map.put(c, frequency+1 );
+	    }
+
+	    return map;
+	}
+	//runtime O(3*n) = O(n)
+	static boolean compare(String s1, String s2){
+	    if(s1.length() != s2.length()){
+	        return false;
+	    }
+
+	    //runtime O(n)
+	    HashMap<Character, Integer> map1 = getFrequencys(s1);
+	    HashMap<Character, Integer> map2 = getFrequencys(s2);
+
+	    //Iterate over every character in map1 (every character contained in s1)  (runtime O(n) )
+	    for(Character c : map1.keySet()){
+	        //if the characters frequencys are different, the strings arent permutations
+	        if( map2.get(c) != map1.get(c)){
+	            return false;
+	        }
+	    }
+
+	    //since every character in s1 has the same frequency in s2,
+	    //and the number of characters is equal => s2 must be a permutation of s1
+
+	    return true;
+	}
+	
 }
