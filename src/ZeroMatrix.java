0c2fca627cfcffa68c819b2b9951afacb6ffbc37
Mannuru <arunm057@gmail.com>
2017-08-18 22:03:04 -0400

    update

--- src/ZeroMatrix.java
+++ src/ZeroMatrix.java
@@ -30,35 +30,44 @@ public class ZeroMatrix {
 		int[][] arrayMatrix = new int[][]{{1,2,3},{5,6,0}};
 		zeroMatrix(arrayMatrix);
 	}
-	static int[][] zeroMatrix(int[][] zeroMatrix){
+	static void zeroMatrix(int[][] zeroMatrix){
 		
-		boolean[] row = new boolean[zeroMatrix.length];
-		boolean[] col = new boolean[zeroMatrix[0].length];
+		boolean[] rowMatrix = new boolean[zeroMatrix.length];
+		boolean[] colMatrix = new boolean[zeroMatrix[0].length];
 		
 		for(int i=0; i < zeroMatrix.length; i++){
 			for(int j=0; j < zeroMatrix[0].length; j++){
 				if(zeroMatrix[i][j] == 0){
-					row[i] = true;
-					col[j] = true;
+					rowMatrix[i] = true;
+					colMatrix[j] = true;
 				}
 			}
 		}
-		for(int i=0; i < row.length; i++){
-			if(row[i]) nullifyRow(zeroMatrix,i);
-		}
-		for(int j=0; j < col.length; j++){
-			if(col[j]) nullifyCol(zeroMatrix,j);
-		}
-		return zeroMatrix;
-	}
-	static void nullifyRow(int[][] zeroMatrix,int row){
-		for(int j=0; j < zeroMatrix.length; j++){
-			zeroMatrix[row][j]=0;
-		}
-	}
-	static void nullifyCol(int[][] zeroMatrix,int col){
+		// identify the row index in matrix to nullify
 		for(int i=0; i < zeroMatrix.length; i++){
-			zeroMatrix[i][col] = 0;
+			if(rowMatrix[i])	
+				nullifyRow(zeroMatrix, i);
+		}
+		// identify the coloumn index in matrix to nullify
+		for(int j=0; j < zeroMatrix[0].length; j++){
+			if(colMatrix[j])	
+				nullifyCol(zeroMatrix,j);
+		}
+		for(int k=0; k < zeroMatrix.length; k++){
+			for(int l=0; l < zeroMatrix[0].length; l++){
+				System.out.println(zeroMatrix[k][l]);
+			}
 		}
 	}
+	static void nullifyRow(int[][] matrix,int row){
+		for(int j=0; j < matrix[0].length; j++){
+			matrix[row][j] = 0;
+		}
+	}
+	static void nullifyCol(int[][] matrix,int col){
+		for(int i=0; i < matrix.length; i++){
+			matrix[i][col] = 0;
+		}
+	}
+	
 }
